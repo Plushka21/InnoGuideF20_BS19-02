af3f0939142487ffa8643aeb3af0a3169882b971
Plushka21 <albugg2001@gmail.com>
2020-10-21 16:24:39 +0500

    New code using Kotlin

---
+++ InnoGuides/app/src/main/java/com/example/innoguides/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.innoguides
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClick(view: View) {
+        Toast.makeText(this, "Button", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
+    }
+}
