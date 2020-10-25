9ca358642820500c0aaba2a3eac5f81aa9d34a7a
Plushka21 <albugg2001@gmail.com>
2020-10-25 22:12:52 +0500

    InnoGuides
    Owrking authoriazation, geolocation and information about places

---
+++ InnoGuidesJava/app/src/main/java/com/example/innoguidesjava/InfoWindow.java
@@ -0,0 +1,33 @@
+package com.example.innoguidesjava;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Build;
+import android.os.Bundle;
+
+public class InfoWindow extends DialogFragment {
+
+    private final String name;
+    private final double rating;
+
+    public InfoWindow(String name, double rating){
+        this.name=name;
+        this.rating=rating;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        return builder
+                .setTitle("Place Info")
+                .setView(R.layout.activity_info_window)
+                .setPositiveButton("OK", null)
+                .setMessage("Name: " + this.name+"\nRating: "+this.rating)
+                .create();
+    }
+}
---
+++ InnoGuidesJava/app/src/main/java/com/example/innoguidesjava/MyLocation.java
@@ -0,0 +1,186 @@
+package com.example.innoguidesjava;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyLocation extends AppCompatActivity
+        implements
+        OnMyLocationButtonClickListener,
+        OnMyLocationClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
+
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean permissionDenied = false;
+    private GoogleMap map;
+    private List<Place> places;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        places = new ArrayList<>();
+        try{
+            places = JSONHelper.readPlaceJSONFile(this);
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_location);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        map.setOnMyLocationButtonClickListener(this);
+        map.setOnMyLocationClickListener(this);
+        enableMyLocation();
+
+        for (int i = 0; i < places.size(); i++){
+            map.addMarker(new MarkerOptions()
+                    .position(new LatLng(places.get(i).getC1(), places.get(i).getC2()))
+                    .title(places.get(i).getName()));
+        }
+
+        LatLng inno = new LatLng(55.75, 48.73);
+        map.addMarker(new MarkerOptions().position(inno).title("Marker in Innopolis"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(inno));
+        moveCamera(inno);
+
+        map.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener(){
+        @Override
+        public boolean onMarkerClick(Marker marker) {
+            String name = marker.getTitle();
+            int index = 0;
+            for (int i = 0; i < places.size(); i++){
+                if (places.get(i).getName().equals(name)){
+                    index = i;
+                    break;
+                }
+            }
+            showDialog(name, places.get(index).getRating());
+            return false;
+        }
+        });
+    }
+
+    /**
+     * Enables the My Location layer if the fine location permission has been granted.
+     */
+    private void enableMyLocation() {
+        // [START maps_check_location_permission]
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            if (map != null) {
+                map.setMyLocationEnabled(true);
+            }
+        } else {
+            // Permission to access the location is missing. Show rationale and request permission
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        }
+        // [END maps_check_location_permission]
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    // [START maps_check_location_permission_result]
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults, Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Permission was denied. Display an error message
+            // [START_EXCLUDE]
+            // Display the missing permission error dialog when the fragments resume.
+            permissionDenied = true;
+            // [END_EXCLUDE]
+        }
+    }
+    // [END maps_check_location_permission_result]
+
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (permissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            permissionDenied = false;
+        }
+    }
+
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
+
+    void moveCamera(LatLng latLng){
+        float zoom = 16f;
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    public void showDialog(String name, double rating) {
+        InfoWindow dialog = new InfoWindow(name, rating);
+        dialog.show(getSupportFragmentManager(), "custom");
+    }
+
+}
